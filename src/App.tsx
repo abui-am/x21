0470ddd6350cca1ca88bcf35e76d7304951fa75c
abui <adjiem31@gmail.com>
2024-04-26 19:46:41 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,79 @@
+import './App.css';
+
+function App() {
+  return (
+    <div
+      style={{
+        maxWidth: 600,
+      }}
+    >
+      <h1>28 Reason Why We Should Try To Date</h1>
+      <div>
+        <ol>
+          <li>You are gorg💅</li>
+          <li>You are Smart (me too)</li>
+          <li>You are Beatiful</li>
+          <li>You are Sweet</li>
+          <li>You have a beatiful eyes</li>
+          <li>You are a hard worker (me too)</li>
+          <li>We have something that we tried to heal</li>
+          <li>We both have a same background</li>
+          <li>We both trying to write our life story </li>
+          <li>We both came from aspiring middle class family</li>
+          <li>We both did not have that much free time</li>
+          <li>We both work in tech</li>
+          <li>We both love to work in tech</li>
+          <li>We can grow together (as a person and a professional)</li>
+          <li>
+            We can support each other (emotionally and financially [and
+            technically])
+          </li>
+          <li>We love to socialize but also appreciate spending time alone</li>
+          <li>We both have same corporate slave vibes</li>
+          <li>We both have same corporate slave vibes 💀</li>
+          <li>Our lives consist of aligning task, doing task, repeat</li>
+          <li>I like how you spent your money wisely</li>
+          <li>I like how you act more mature than your ages</li>
+          <li>
+            We lowkey have insecurities at work about our ages, and not so high
+            education background. But we are working on it.
+          </li>
+          <li>We can go to conf together</li>
+          <li>Maybe hackathon dates (?)</li>
+          <li>We both have a same dream (banging 2 digits salary)</li>
+          <li>
+            We both have a dream to have normal college life, but we just cant.
+          </li>
+          <li>The world setup us to fail, but nah we'd win</li>
+          <li>
+            I know you are living without safety net, so you can't commit a time
+            for something like this. but me too. Thats why you overwork so much,
+            and I know that. Living in hardmode is hard, but we can do it.
+          </li>
+        </ol>
+
+        <div>
+          <h3>If this line resonates with you, please consider my offer.</h3>
+          <ol>
+            <li>
+              "You dont throw our youth, you just did not have it at the moment.
+              You must create it first."
+            </li>
+            <li>
+              "My son/daughter will be either international class student, LPDP
+              awardee, foreign exchange student, or Cumlaude at UI or another
+              Top Uni, having access to anything he/she want. He/She will enjoy
+              his college life, and he will have a good job."
+            </li>
+            <li>
+              "I often worry about my future, being poor is harder than working
+              6 days a week."
+            </li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
